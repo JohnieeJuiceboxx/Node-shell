73bbd10feec6fa5efe531f488a19e1b45d0de400
Harsh Patel <harshp97@gmail.com>
2019-08-05 12:09:45 -0400

    ls function

--- bash.js
+++ bash.js
@@ -1,11 +1,11 @@
 const pwdFunction = require('./pwd');
-
+const lsFunction = require('./ls');
+const fs = require('fs');
 process.stdout.write('prompt >');
 
 process.stdin.on('data', data => {
   const cmd = data.toString().trim();
-
   pwdFunction(cmd);
+  lsFunction(fs);
   //process.stdout.write('You typed: ' + cmd);
-  process.stdout.write('\nprompt >');
 });
